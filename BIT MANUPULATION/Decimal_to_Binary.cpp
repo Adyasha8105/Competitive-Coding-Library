1d03ba6d0d5cc4de32a138c7a19c2cbd6c8cec46
Adyasha Mohanty <adyashamohanty8105@gmail.com>
2020-12-25 13:01:50 +0530

    Decimal to Binary conversion

---
+++ BIT MANUPULATION/Decimal_to_Binary.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+//Decimal to binary conversion
+
+int decimaltobinary(int n){
+
+    int ans=0;
+    int p =1;
+    while(n>0){
+        int last_bit = (n&1);
+        ans+= p*last_bit;
+        p = p*10;
+        n=n>>1;
+    }
+    return ans;
+}
+int main() {
+    int n;
+    cin>>n;
+    cout<<decimaltobinary(n);
+    return 0;
+}
