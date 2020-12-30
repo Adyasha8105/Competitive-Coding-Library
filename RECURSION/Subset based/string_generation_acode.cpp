31e48864ecf0cd79b579fbb2dc173734f4bee68e
Adyasha Mohanty <adyashamohanty8105@gmail.com>
2020-12-31 00:47:34 +0530

    string generation code

---
+++ RECURSION/Subset based/string_generation_acode.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+//string generation
+
+void generate_strings(char *in, char *out, int i,int j){
+   //base case
+   if(in[i]=='\0'){
+       out[j]='\0';
+       cout<<out<<endl;
+       return;
+   }
+   
+   //rec case
+   //one digit at time
+   int digit = in[i] - '0';
+   char ch = digit +'A' - 1;
+   out[j]=ch;
+   generate_strings(in,out,i+1,j+1);
+
+   //two digits at time
+   if(in[i+1]!='\0'){
+       int secondDigit = in[i+1] - '0';
+       int no= digit*10 + secondDigit;
+       if(no<=26){
+            ch = no +'A' - 1;
+            out[j]=ch;
+            generate_strings(in,out,i+2,j+1);
+       }
+   }
+   return;
+}
+int main() {
+    char a[100];
+    cin>>a;
+
+    char out[100];
+    generate_strings(a,out,0,0);
+  return 0;
+}
